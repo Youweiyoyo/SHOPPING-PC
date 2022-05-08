70973d1cd44b1e7ca4f73d457822b081b3abfb52
Youwei <yoyoyo9527@163.com>
2022-05-08 19:00:02 +0800

    feat：排序逻辑完成

--- src/view/Category/category.vue
+++ src/view/Category/category.vue
@@ -5,15 +5,25 @@
       <SubBread />
       <!-- 二级分类展示区 -->
       <SubFilter />
-      <PCCheckBox v-model="isCheckBoxAll">全选</PCCheckBox>{{isCheckBoxAll}}
+      <!-- 商品面板 排序 + 列表 -->
+      <div class="goods-list">
+        <SubSort />
+      </div>
+
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {ref} from 'vue'
-const isCheckBoxAll = ref(false)
+import SubSort from './components/sub-sort.vue'
+import SubFilter from './components/sub-filter.vue'
+import SubBread from './components/sub-bread.vue'
 </script>
 
 <style scoped lang="less">
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+}
 </style>
